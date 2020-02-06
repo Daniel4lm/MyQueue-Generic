3ada71f6c02da5032d712cb124cff16173bcef0b
Daniel Molnar <daniel4molnar@gmail.com>
2020-02-06 23:15:54 +0100

    Dodata metoda add(index, T element), toString() metod i implementacija Iterable interfejsa za koristenje u forEach petljama

--- src/mylist/TestMyList.java
+++ src/mylist/TestMyList.java
@@ -3,18 +3,18 @@ package mylist;
 import java.util.Arrays;
 import java.util.Iterator;
 
-class MyList<E> {
+class MyList<T> implements Iterable<T> {
 
 	/**
 	 * 
-	 *  Genericka klasa MyList
-	 *  
-	 *  @author Daniel M
+	 * Genericka klasa MyList
+	 * 
+	 * @author Daniel M
 	 * 
 	 */
-	
-	/**  Inicijalna kolicina elemenata, inicijalni kapacitet */
-	
+
+	/** Inicijalna kolicina elemenata, inicijalni kapacitet */
+
 	private int size = 0;
 
 	private int initialCapacity = 10;
@@ -22,20 +22,21 @@ class MyList<E> {
 	private int oldCapacity = initialCapacity;
 
 	private int newCapacity;
-	
-	/** 
+
+	/**
 	 * 
 	 * Niz elemenata kolekcije
 	 * 
-	 * */	
+	 */
+
 	private Object[] myElements;
 
 	/**
 	 * 
-	 *  Bazni konstruktor koji inicijalizira niz elemanata sa inicijalnim kapacitetom
-	 *  
-	 * */
-	
+	 * Bazni konstruktor koji inicijalizira niz elemanata sa inicijalnim kapacitetom
+	 * 
+	 */
+
 	public MyList() {
 
 		myElements = new Object[initialCapacity];
@@ -44,13 +45,14 @@ class MyList<E> {
 
 	/**
 	 * 
-	 *  Metod za dodavanje lemenata na kraj niza
-	 *  
+	 * Metod za dodavanje lemenata na kraj niza
+	 * 
 	 */
-	
-	public void add(E element) {
-		//System.out.println("Size je: " + size + " stv duzina: " + ((int)(myElements.length * 0.8)));
-		if (size >= (int)(myElements.length * 0.8)) {
+
+	public void add(T element) {
+		// System.out.println("Size je: " + size + " stv duzina: " +
+		// ((int)(myElements.length * 0.8)));
+		if (size >= (int) (myElements.length * 0.8)) {
 			increaseCapacity();
 		}
 
@@ -58,40 +60,55 @@ class MyList<E> {
 
 	}
 
-	public void add(int index, E element) {
+	public void add(int index, T element) {
 
-		// ...
+		if (index < 0 || index > myElements.length) {
+			IndexOutOfBoundsException indOutOfBoEx = new IndexOutOfBoundsException(
+					"Izasli ste iz okvira kolekcije! -> size : " + size + " -> index " + index + " (?)");
+			throw indOutOfBoEx;
+		} else if (size >= (int) (myElements.length * 0.8)) {
+			increaseCapacity();
+		}
+
+		Object[] tempArray_to = Arrays.copyOfRange(myElements, 0, index);
+		Object[] tempArray_from = Arrays.copyOfRange(myElements, index, size);
+
+		myElements = Arrays.copyOf(tempArray_to, size + 1);
+		myElements[index] = element;
+
+		System.arraycopy(tempArray_from, 0, myElements, index + 1, tempArray_from.length);
+		size++;
 
 	}
-	
+
 	/**
 	 * 
-	 *  Metod za dobijanje elementa na poziciji indeks parametra
+	 * Metod za dobijanje elementa na poziciji indeks parametra
 	 * 
 	 * @param index
 	 * @return E
 	 * 
 	 */
-	
-	public E get(int index) {
-		
-		if(index < 0 || index > myElements.length) {
+
+	public T get(int index) {
+
+		if (index < 0 || index > myElements.length) {
 			IndexOutOfBoundsException indOutOfBoEx = new IndexOutOfBoundsException(
 					"Izasli ste iz okvira kolekcije! -> size : " + size + " -> index " + index + " (?)");
 			throw indOutOfBoEx;
 		}
-		
-		return (E) myElements[index];		
+
+		return (T) myElements[index];
 	}
 
 	/**
 	 * 
-	 *  Metod za uklanjanje elementa sa pozicije indeks parametra
+	 * Metod za uklanjanje elementa sa pozicije indeks parametra
 	 * 
 	 * @param index
 	 * 
 	 */
-	
+
 	public void remove(int index) {
 
 		if (index < 0) {
@@ -104,25 +121,24 @@ class MyList<E> {
 
 		Object[] tempArray_to = Arrays.copyOfRange(myElements, 0, index);
 		Object[] tempArray_from = Arrays.copyOfRange(myElements, index + 1, size);
-			
-		myElements = Arrays.copyOf(tempArray_to, size);		
-		
+
+		myElements = Arrays.copyOf(tempArray_to, size);
+
 		System.arraycopy(tempArray_from, 0, myElements, index, tempArray_from.length);
 		size--;
-		// ...
 
 	}
 
 	/**
 	 * 
-	 *  Metod za vracanje velicine niza
+	 * Metod za vracanje velicine niza
 	 * 
 	 */
-	
+
 	public int size() {
 
 		return size;
-		
+
 	}
 
 	public boolean isEmpty() {
@@ -141,7 +157,52 @@ class MyList<E> {
 
 	}
 
-	// the rest of other methods ...
+	/**
+	 * 
+	 * Implementacija Iterable metode za dobijanje iteratora za elemente niza
+	 * 
+	 */
+
+	@Override
+	public Iterator<T> iterator() {
+
+		return (Iterator<T>) Arrays.asList(Arrays.copyOf(myElements, size)).iterator();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.deepHashCode(myElements);
+		result = prime * result + size;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MyList other = (MyList) obj;
+		if (!Arrays.deepEquals(myElements, other.myElements))
+			return false;
+		if (size != other.size)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+
+		String toStringOutput = Arrays.toString(Arrays.copyOf(myElements, size));
+		toStringOutput = toStringOutput.replaceAll("\\[", "< ");
+		toStringOutput = toStringOutput.replaceAll("\\]", " >");
+
+		return toStringOutput;
+	}
 
 }
 
@@ -150,38 +211,47 @@ public class TestMyList {
 	public static void main(String[] args) {
 
 		MyList<Integer> mojaLista = new MyList<>();
-		
+
 		System.out.println(mojaLista.size());
-		
+
 		mojaLista.add(14);
 		mojaLista.add(10);
 		mojaLista.add(54);
 		mojaLista.add(24);
-		mojaLista.add(null);
+		// mojaLista.add(null);
 		mojaLista.add(4);
-		
+
 		System.out.println(mojaLista.size());
-				
+
 		for (int i = 0; i < mojaLista.size(); i++) {
-			 System.out.print(mojaLista.get(i) + " ");
+			System.out.print(mojaLista.get(i) + " ");
 		}
 		System.out.println();
-		
+
 		mojaLista.remove(2);
 		System.out.println(mojaLista.size());
-		
-		for (int i = 0; i < mojaLista.size(); i++) {
-			 System.out.print(mojaLista.get(i) + " ");
-		}System.out.println();
-		
+
+		for (Integer it : mojaLista) {
+			System.out.print(it + " ");
+		}
+		System.out.println();
+
+		System.out.println(mojaLista.toString());
+
 		for (int i = 0; i < 15; i++) {
-			 mojaLista.add(i+1);
+			mojaLista.add(i + 1);
 		}
+
+		for (Integer it : mojaLista) {
+			System.out.print(it + " ");
+		}
+		System.out.println();
+
+		mojaLista.add(8, 84);
 		
 		for (int i = 0; i < mojaLista.size(); i++) {
-			 System.out.print(mojaLista.get(i) + " ");
+			System.out.print(mojaLista.get(i) + " ");
 		}
-		
 	}
 
 }
